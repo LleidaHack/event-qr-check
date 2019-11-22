2039d4f8116f69a7460f40854c4d06ee6be263bb
Víctor Martínez Montané <victorcoder2@gmail.com>
2019-11-22 01:16:55 +0100

    Block device orientation

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,17 @@ import 'package:event_qr_check/routes.dart';
 import 'package:event_qr_check/screens/splash/splash_screen.dart';
 import 'package:flutter/material.dart';
 
-main() => runApp(
-  MaterialApp(
-    home: SplashScreen(),
-    theme: ThemeData(fontFamily: 'Montserrat'),
-    routes: Router.routes,
-  )
-);
+main() async {
+  await SystemChrome.setPreferredOrientations([
+    //Blocks the device orientation
+    DeviceOrientation.portraitUp,
+  ]);
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+      theme: ThemeData(fontFamily: 'Montserrat'),
+      routes: Router.routes,
+    ),
+  );
+}
